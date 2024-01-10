f5f30f002220f05990188d225bf9c5a146ef5c7f
Cezary Malinowski <violex.cmalinowski@gmail.com>
2024-01-10 15:27:14 +0100

    change let to const and remove redundant cout

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
 {const playGame = function(playerInput) {
     clearMessages();
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-    let computerMove = getMoveName(randomNumber);
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
+    const computerMove = getMoveName(randomNumber);
 
     printMessage('Mój ruch to: ' + computerMove);
 
-    // let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-    let playerMove = getMoveName(playerInput);
+    const playerMove = getMoveName(playerInput);
 
     displayResult(computerMove, playerMove);
 }
